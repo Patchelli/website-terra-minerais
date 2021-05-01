0e2ebc613ffbec2eaa3c287b0e3ea96412757641
Lucas Santana <lucas_377@hotmail.com>
2021-04-30 23:43:37 -0300

    Finalizado Menu responsivo.
    + Aplicado técnica para deixar dinamico no desktop.

--- src/js/app.js
+++ src/js/app.js
@@ -12,6 +12,27 @@ $('.menu__overlay').click(() => Menu.toggleMenu());
 $('.menu__mobile__btn').click(() => Menu.toggleMenu());
 
 /*------------------------
+Menu Dinamico
+------------------------*/
+$(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 80){
+            $("header").css('background-color', 'black');
+            $("header").css('padding-top', '10px');
+            $("header").css('padding-bottom', '10px');
+            $(".menu__logo img").css('height', '50px');
+        }
+
+        else {
+            $("header").css('background-color', 'transparent');
+            $(".menu__logo img").css('height', '85px');
+            $("header").css('padding-top', '30px');
+            $("header").css('padding-bottom', '0px');
+        }
+    });
+});
+
+/*------------------------
 Home Carousel
 ------------------------*/
 $('.banner__carousel').slick({
